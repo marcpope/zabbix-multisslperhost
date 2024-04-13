c30757ba40a7c5b0c055cd6d46e1211055fc5f00
Marc Pope <marcpope@me.com>
2024-04-12 23:01:29 -0400

    Create ssl.sh
    Scans an Apache conf.d directory for any VirtualHost block that uses port 443 and exports as JSON in the format that Zabbix likes. It's only searching for the primary VirtualHost name.

---
+++ ssl.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+dir="/etc/httpd/conf.d"
+pattern="vhost*.conf"
+
+read -r -d '' awkScript <<'EOF'
+/<VirtualHost.*:443>/ {
+  inBlock=1
+}
+/\/VirtualHost>/ {
+  if (inBlock && foundServerName) {
+    print serverName
+    foundServerName=0
+  }
+  inBlock=0
+}
+{ if (inBlock && /ServerName/) {
+    serverName = $2
+    foundServerName=1
+  }
+}
+EOF
+
+serverNames=()
+
+for file in "$dir"/$pattern; do
+  while IFS= read -r line; do
+    serverNames+=("$line")
+  done < <(awk "$awkScript" "$file")
+done
+
+# Generate JSON output for Zabbix
+echo -n '{"data":['
+for domain in "${serverNames[@]}"; do
+    echo -n "{\"{#SSLDOMAIN}\":\"$domain\"},"
+done | sed 's/,$//'
+echo -n ']}'
